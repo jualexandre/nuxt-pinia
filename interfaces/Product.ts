e81db112c29509eed78792e1e1552c19c6fe1613
Julien <contact@julienalexandre.com>
2025-01-16 13:54:56 +0100

    chore: creation of product cards

---
+++ interfaces/Product.ts
@@ -0,0 +1,7 @@
+export interface Product {
+  title: string
+  price: number
+  description: string
+  img: string
+  id: number
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,21 @@
 <template>
   <div>
     <h2 class="text-white text-2xl text-center my-8">All Products</h2>
+
+    <div class="grid grid-cols-2 gap-7">
+      <div v-for="product in products">
+        <ProductCard :product="product" />
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// Fetch products from the API
+import type { Product } from '~/interfaces/Product'
+
+const { data: products } = await useFetch<Product[]>('http://localhost:3001/products')
+console.log(products)
+</script>
 
 <style scoped></style>
